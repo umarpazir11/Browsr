d8c29a131d8ddf9cf124610a3c142c5a0361f0cf
Umer <umarpazir@gmail.com>
2020-02-07 22:49:39 +0100

    Second test Commit of Project

--- app/src/main/java/com/zenjob/android/browser/MainActivity.kt
+++ app/src/main/java/com/zenjob/android/browser/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.zenjob.android.browser
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,6 +10,6 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
-
+        Toast.makeText(this,"Test",Toast.LENGTH_LONG).show()
     }
 }
